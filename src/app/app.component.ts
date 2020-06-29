f1d08802d79b23c32ef2353cc9ad7dd994385fe6
Omi1 <anthonyangatia@gmail.com>
2020-06-29 10:10:57 +0300

    Base Project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 
+import { Rellax } from 'Rellax';
+declare var Rellax: any;
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'nemifx4';
+  rellax = new Rellax('.rellax');
 }
