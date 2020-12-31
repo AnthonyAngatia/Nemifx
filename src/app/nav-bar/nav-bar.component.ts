68f597eccaf5704b0484e76aa03252c9af4e8a61
unknown <anthonyangatia@gmail.com>
2021-01-01 00:06:48 +0300

    Fixing git

--- src/app/nav-bar/nav-bar.component.ts
+++ src/app/nav-bar/nav-bar.component.ts
@@ -4,50 +4,48 @@ import { MAT_DRAWER_CONTAINER } from '@angular/material/sidenav/drawer';
 @Component({
   selector: 'app-nav-bar',
   template: `
-  
+
   <mat-drawer #drawer >
     <button mat-icon-button class="example-icon" aria-label="Close" (click)="drawer.toggle()">
       <mat-icon >close</mat-icon>
     </button>
     <nav class="nav_links_side">
       <ul>
-      <li>
-      <a routerLink ="/uploads">Recent Posts</a>
-    </li>
-      <li>
-        <a href="https://t.me/nemifx_moneymining" target="_blank" rel="noopener noreferrer">Telegram Group</a>
-      </li>
-</ul>
-
-    </nav>     
+        <li>
+          <a routerLink ="/uploads">Recent Posts</a>
+        </li>
+        <li>
+          <a href="https://t.me/nemifx_moneymining" target="_blank" rel="noopener noreferrer">Telegram Group</a>
+        </li>
+      </ul>
+    </nav>
   </mat-drawer>
   <!-- Desktop Navigation view -->
   <mat-toolbar class="mat-toolbar">
+    <button  class="burger-btn" mat-icon-button  aria-label="Example icon-button with menu icon" (click)="drawer.toggle()" >
+      <mat-icon>menu</mat-icon>
+    </button>
 
-  <button  class="burger-btn" mat-icon-button  aria-label="Example icon-button with menu icon" (click)="drawer.toggle()" >
-    <mat-icon>menu</mat-icon>
-  </button>
-
-  <span class="nav_spacer_2">
-    <a routerLink="/"> <img class="logo" src="assets/nemilogo.png" alt="Logo"/> </a>
-  </span>
-  <!--  Navigation links on Right Side -->
-  <span class ="nav_spacer_1" style="flex: 1 1 auto">
-  <a routerLink="/"><b> Nemi Fx</b> </a>
-</span>
-  <span class ="nav_spacer_2" style="flex: 1 1 auto"></span>
-  <nav class="nav_links">
-    <ul>
-            <li><a routerLink ="/uploads" style="color:white">Recent Posts </a></li>
-            <li><a href="https://t.me/nemifx_moneymining" target="_blank" rel="noopener noreferrer" style="color:white">Telegram Group</a></li>
-            <!-- <li class="analysis" >Analysis</li> -->
-            <!-- <li>Crypto markets</li> -->
-            <!-- <li>Community</li> -->
-</ul>
-  </nav>
+    <span class="nav_spacer_2">
+      <a routerLink="/"> <img class="logo" src="assets/nemilogo.png" alt="Logo"/> </a>
+    </span>
+    <!--  Navigation links on Right Side -->
+    <span class ="nav_spacer_1" style="flex: 1 1 auto">
+      <a routerLink="/"><b> Nemi Fx</b> </a>
+    </span>
+    <span class ="nav_spacer_2" style="flex: 1 1 auto"></span>
+    <nav class="nav_links">
+      <ul>
+          <li><a routerLink ="/uploads" style="color:white">Recent Posts </a></li>
+          <li><a href="https://t.me/nemifx_moneymining" target="_blank" rel="noopener noreferrer" style="color:white">Telegram Group</a></li>
+          <!-- <li class="analysis" >Analysis</li> -->
+          <!-- <li>Crypto markets</li> -->
+          <!-- <li>Community</li> -->
+      </ul>
+    </nav>
 </mat-toolbar>
 
-    
+
   `,
   styleUrls: ['./nav-bar.component.css']
 })
